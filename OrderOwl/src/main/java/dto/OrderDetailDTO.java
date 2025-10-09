010486b79ca18214e0df347b1bf612de7410f867
SeokYong <132437309+Yoon0917@users.noreply.github.com>
2025-10-09 18:24:16 +0900

    create DTO

---
+++ OrderOwl/src/main/java/dto/OrderDetailDTO.java
@@ -0,0 +1,9 @@
+package dto;
+
+public class OrderDetailDTO {
+	private int orderDetailId;
+    private int orderId;
+    private int menuId;
+    private int quantity;
+    private int price;
+}
---
+++ OrderOwl/src/main/java/dto/QrcodeDTO.java
@@ -0,0 +1,11 @@
+package dto;
+
+import java.time.LocalDateTime;
+
+public class QrcodeDTO {
+	 private int qrcodeId;
+	    private int tableId;
+	    private String qrcodeData;
+	    private String qrImgSrc;
+	    private LocalDateTime createdAt;
+}
